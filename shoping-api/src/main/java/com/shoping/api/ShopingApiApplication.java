85e6d63605da278d05edde5fa5d7ca95d3814cbd
davidrusyki <davidrusyck@gmail.com>
2022-10-08 16:45:01 -0300

    reestruturando as pastas

---
+++ shoping-api/src/main/java/com/shoping/api/ShopingApiApplication.java
@@ -0,0 +1,13 @@
+package com.shoping.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingApiApplication.class, args);
+	}
+
+}
