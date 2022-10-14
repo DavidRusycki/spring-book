61def582b6f1993748bb5dc407d828b5e1c09760
Samuca23 <samuelchiodini@hotmail.com>
2022-10-14 20:59:34 -0300

    fim

---
+++ shoping-api/src/main/java/com/shoping/api/dto/ShopDTO.java
@@ -0,0 +1,60 @@
+package com.shoping.api.dto;
+
+import java.util.Date;
+import java.util.List;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import com.shoping.api.entity.Shop;
+
+public class ShopDTO {
+	@NotBlank
+	private String userIdentifier;
+	@NotNull
+	private Float total;
+	@NotNull
+	private Date date;
+	@NotNull
+	private List<ItemDTO> items;
+
+// get and sets
+	public static ShopDTO convert(Shop shop) {
+		ShopDTO shopDTO = new ShopDTO();
+		shopDTO.setUserIdentifier(shop.getUserIdentifier());
+		shopDTO.setTotal(shop.getTotal());
+		return shopDTO;
+	}
+
+	public String getUserIdentifier() {
+		return userIdentifier;
+	}
+
+	public void setUserIdentifier(String userIdentifier) {
+		this.userIdentifier = userIdentifier;
+	}
+
+	public Float getTotal() {
+		return total;
+	}
+
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public List<ItemDTO> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemDTO> items) {
+		this.items = items;
+	}
+	
+	
+}
